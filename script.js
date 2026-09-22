063abd3fdf9a928b6f9c8d54d580b3149407c9d2
TC-Wei <happy533177@gmail.com>
2026-09-22 17:22:53 +0800

    feat: ⌫ 退格

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(".card-body").on("click", (e) => {
   const $btn = $(e.target);
   const $screen = $(".screen");
-  if ($btn.text() === "C") {
+  if ($btn.text() === "⌫") {
+    $screen.text($screen.text().slice(0, -1));
+  } else if ($btn.text() === "C") {
     $screen.text("0");
   } else if ($btn.hasClass("btn")) {
     if ($screen.text() === "0") {
