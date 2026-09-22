f350db8e38f4741c56945f18603ab6129dcd3c23
TC-Wei <happy533177@gmail.com>
2026-09-22 17:28:03 +0800

    feat: +/- 正負號切換

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(".card-body").on("click", (e) => {
   const $btn = $(e.target);
   const $screen = $(".screen");
-  if ($btn.text() === "⌫") {
-    $screen.text($screen.text().slice(0, -1));
-  } else if ($btn.text() === "C") {
+  if ($btn.text() === "C") {
     $screen.text("0");
+  } else if ($btn.text() === "⌫") {
+    $screen.text($screen.text().slice(0, -1));
+  } else if ($btn.text() === "+/-") {
+    $screen.text($screen.text() * -1);
   } else if ($btn.hasClass("btn")) {
     if ($screen.text() === "0") {
       $screen.text($btn.text());
