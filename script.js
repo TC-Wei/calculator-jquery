8708a043ace702fd108ae9891e05acd004b02908
TC-Wei <happy533177@gmail.com>
2026-09-23 10:49:15 +0800

    feat: 運算子與等號，四則運算

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let firstNumber;
+let operator;
 $(".card-body").on("click", (e) => {
   const $btn = $(e.target);
   const $screen = $(".screen");
+
   if ($btn.text() === "C") {
     $screen.text("0");
   } else if ($btn.text() === "⌫") {
@@ -9,6 +12,21 @@ $(".card-body").on("click", (e) => {
     $screen.text($screen.text() * -1);
   } else if ($btn.text() === "%") {
     $screen.text($screen.text() * 0.01);
+  } else if ($btn.text() === "=") {
+    const secondNumber = Number($screen.text());
+    if (operator === "+") {
+      $screen.text(firstNumber + secondNumber);
+    } else if (operator === "-") {
+      $screen.text(firstNumber - secondNumber);
+    } else if (operator === "×") {
+      $screen.text(firstNumber * secondNumber);
+    } else if (operator === "÷") {
+      $screen.text(firstNumber / secondNumber);
+    }
+  } else if ($btn.hasClass("operator")) {
+    firstNumber = Number($screen.text());
+    operator = $btn.text();
+    $screen.text("");
   } else if ($btn.hasClass("btn")) {
     if ($screen.text() === "0") {
       $screen.text($btn.text());
